046ad59517fc46cccff15a128db79e56e0a67d80
1915387 <1915387@01-19636.pusd.dom>
2023-05-11 10:09:59 -0700

    First commit

---
+++ TakeHomeQuiz1/src/WeatherData.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class WeatherData {
+	private Stack<Double> temperatures;
+	
+	public void cleanData(double lower, double upper) {
+		
+	}
+	
+	public int longestHeatWave(double threshold) {
+		
+	}
+}
